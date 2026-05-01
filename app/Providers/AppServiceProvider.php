a4d730db176c8259f68d62e027cbf82bb648664e
-_- <marygracerefugia2004@gmail.com>
2026-05-01 16:07:02 +0800

    fix string length for MySQL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,13 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use App\Models\Order;
 use App\Policies\OrderPolicy;
 
 class AppServiceProvider extends ServiceProvider {
     public function boot(): void {
+        Schema::defaultStringLength(191);
         Gate::policy(Order::class, OrderPolicy::class);
     }
 }
